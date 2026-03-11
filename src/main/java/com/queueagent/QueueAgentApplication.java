81b6a0cf4219d659e59e5ca780be27deda040149
지선 최 <paper138@naver.com>
2026-03-11 23:59:39 +0900

    init: spring boot 프로젝트 초기 세팅

---
+++ src/main/java/com/queueagent/QueueAgentApplication.java
@@ -0,0 +1,13 @@
+package com.queueagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueAgentApplication.class, args);
+    }
+
+}
